243b1532a0fc21962339ebfb29e3839dd6746495
Edgar Munoz Q <edgmuo1085@gmail.com>
2025-09-04 14:41:37 -0500

    se implementa iframe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'super-set';
+  enlace: string = 'http://localhost:8088/superset/dashboard/p/2Oq9abDXzVk/';
 }
