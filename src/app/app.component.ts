3e90327e288de8984298756aa4c55d37d9f628e4
Edgar Munoz Q <edgmuo1085@gmail.com>
2025-09-04 15:37:24 -0500

    se ajusta parametros para despliegue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,13 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 })
 export class AppComponent implements OnInit {
   title = 'super-set';
-  enlace: string = 'http://localhost:8088/superset/dashboard/p/2Oq9abDXzVk/';
+  enlace: string = 'https://www.catastroantioquia.co/CentroControl/app/6';
+  //enlace: string = 'http://localhost:8088/superset/dashboard/p/2Oq9abDXzVk/';
   urlRedirect: SafeResourceUrl = '';
   private readonly domSanitizer = inject(DomSanitizer);
 
   ngOnInit(): void {
-    const urlClean = this.domSanitizer.bypassSecurityTrustResourceUrl(
-      this.enlace
-    );
+    const urlClean = this.domSanitizer.bypassSecurityTrustResourceUrl(this.enlace);
     this.urlRedirect = urlClean;
   }
 }
